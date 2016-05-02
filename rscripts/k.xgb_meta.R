818a432d07d39a6f1b2b61838e4cdedf900a43cd
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2016-05-02 21:54:40 +1000

    trigram data set

--- rscripts/k.xgb_meta.R
+++ rscripts/k.xgb_meta.R
@@ -8,14 +8,17 @@ library(caret)
 library(Matrix)
 library(caTools)
 
-class_id <- total[total$hat != -1, 'class_id']
+class_id <- total[total$hat >= 0, 'class_id']
+class_id[is.na(class_id)] <- 1
+table(total[is.na(class_id),'hat'])
 class_id <- as.factor(class_id)
-levels(class_id) <- c(0:29)
-class_id <- as.numeric(class_id)
+levels(class_id) <- c(0:30)
+class_id <- as.numeric(class_id)-1
+table(class_id)
 
 ### Split Data ###
 set.seed(23)
-cv <- 2
+cv <- 5
 folds <- createFolds(class_id, k = cv, list = FALSE)
 dropitems <- c('job_id','obj_hat', extra_feature[96:157])
 feature.names <- colnames(train)[!colnames(train) %in% dropitems] 
@@ -40,7 +43,7 @@ for(i in 1:2){
                      subsample           = .8,
                      colsample           = .4,
                      print.every.n       = 1,
-                     num_class           = 30
+                     num_class           = 31
     )
     cat(paste0('Iteration: ', i, ' || Score: ', 2*(clf$bestScore-0.5)))
     
